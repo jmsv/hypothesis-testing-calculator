9a52f5863d60ebe8c9178838f9a78e3739b0e789
James Vickery <dev@jamesvickery.net>
2017-10-20 10:33:51 +0100

    Added Android solution

---
+++ Android/HypothesisTestingAndroid/SplashActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace HypothesisTestingAndroid
+{
+	using System.Threading;
+
+	using Android.App;
+	using Android.OS;
+
+	[Activity(Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true, Label = "Hypothesis Testing", Icon = "@drawable/htcIcon")]
+	public class SplashActivity : Activity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			//SetContentView (Resource.Layout.splashLayout);
+			base.OnCreate(bundle);
+
+			//Thread.Sleep(100); // Simulate a long loading process on app startup.
+			StartActivity(typeof(HypothesisTestingAndroid.MainActivity));
+		}
+	}
+}
+
